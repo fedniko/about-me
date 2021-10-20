511599431ce8315056258742e94bb06265095c80
fedniko <fednik2011@gmail.com>
2021-10-20 12:48:59 +0900

    feat: install sass

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   reactStrictMode: true,
 }
